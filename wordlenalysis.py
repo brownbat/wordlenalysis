1473a664a0dae7883452a932188d00ab4817625d
Thomas B <brownbat@users.noreply.github.com>
2022-01-31 09:19:55 +0800

    solver

--- wordlenalysis.py
+++ wordlenalysis.py
@@ -1,7 +1,8 @@
 import os
 import sys
 
-# TODO - find best second moves
+# TODO - optimize least squares
+# TODO - store solutions list at each step of solver for debugging
 
 FN_SOLVE = "wordle_solutions_alphabetized.txt"
 FN_GUESS = "wordle_complete_dictionary.txt"
@@ -12,7 +13,13 @@ os.chdir(DIRNAME)
 RESULT_OPTIONS = ["□▣■",
                   "□▤▦",
                   "□▧▩",
-                  " ☆★"]
+                  " ☆★",
+                  " YG"]
+
+# RESULT_OPTIONS[0] means I'm biased for presentation, with the cost
+# that test cases are harder to type. " YG" maybe best here.
+
+
 RESULTS = list(RESULT_OPTIONS[0])
 GRAY, YELLOW, GREEN = RESULTS
 
@@ -28,7 +35,7 @@ with open(FN_GUESS, 'r') as GUESSES_FILE:
         GUESSES.append(guess.strip().upper())
 
 
-def compare(sol, guess):
+def guess_to_hint(sol, guess):
     assert type(guess) == type(sol) == str
     assert len(guess) == len(sol) == 5
     guess = guess.upper()
@@ -62,7 +69,7 @@ def compare(sol, guess):
     return out_str
 
 
-def test_compare():
+def test_guess_to_hint():
     # need more tests.
     # i think abbba,bacon is testing lookahead, but could use more
     tests = [
@@ -80,12 +87,12 @@ def test_compare():
 
     for t in tests:
         try:
-            if compare(t[0], t[1]) == t[2]:
+            if guess_to_hint(t[0], t[1]) == t[2]:
                 print("PASS")
             else:
                 print(t[0])
                 print(t[1])
-                print(compare(t[0], t[1]))
+                print(guess_to_hint(t[0], t[1]))
                 print(t[2])
                 print("FAIL")
         except AssertionError:
@@ -98,7 +105,7 @@ def test_compare():
 def all_patterns(guess, answers):
     patterns = {}
     for sol in answers:
-        p = compare(sol, guess)
+        p = guess_to_hint(sol, guess)
         patterns[p] = patterns.get(p, 0) + 1
     return patterns
 
@@ -127,7 +134,7 @@ def guess_to_sum_squares(guess, answers):
 def answers_guess_hint_to_answers(answers, guess, hint):
     new_answers = []
     for a in answers:
-        if compare(a, guess) == hint:
+        if guess_to_hint(a, guess) == hint:
             new_answers.append(a)
     if new_answers == []:
         print('WARNING: NO MATCHING SOLUTIONS!')
@@ -139,7 +146,8 @@ def least_squares(answers, guesses):
     least_sum_squares = 9999999
     best_guess = "XXXXX"
     for guess in guesses:
-        print(guess + '   CURRENT BEST GUESS: ', best_guess, ' SCORE: ', least_sum_squares, end='\r')
+        print(guess + '   CURRENT BEST GUESS: ', best_guess, ' SCORE: ',
+              least_sum_squares, end='          \r')
         sum_squares = guess_to_sum_squares(guess, answers)
         if sum_squares < least_sum_squares:
             best_guess = guess
@@ -147,15 +155,51 @@ def least_squares(answers, guesses):
     print()
     return (best_guess, least_sum_squares)
 
-# new_ans = answers_guess_hint_to_answers(SOLUTIONS, 'RAISE', GRAY * 5)
-# print(guess_to_hint_counts('RAISE', SOLUTIONS))
+
+def best_second_guess(guess, answers=SOLUTIONS):
+    hint_counts = guess_to_hint_counts(guess, answers)
+    second_guesses = []
+    for hc in hint_counts:
+        hint = hc[0]
+        new_ans = answers_guess_hint_to_answers(answers, guess, hint)
+        second_guesses.append((hint, least_squares(new_ans, GUESSES)[0]))
+    return second_guesses
+
+
+def results_to_answers(guess_hints, answers):
+    gh_stack = guess_hints.copy()
+    new_ans = answers.copy()
+    while len(gh_stack) > 0:
+        gh = gh_stack.pop()
+        guess = gh[0]
+        hint = gh[1]
+        new_ans = answers_guess_hint_to_answers(new_ans, guess, hint)
+    return new_ans
+
+
+def results_to_best_guess(guess_hints, answers):
+    new_ans = results_to_answers(guess_hints, answers)
+    return least_squares(new_ans, GUESSES)[0]
+
+
+def solution_path(answer, first_guess = None):
+    new_ans = SOLUTIONS.copy()
+    gh = []
+    if first_guess:
+        # first guess is massively slower
+        hint = guess_to_hint(answer, first_guess)
+        gh.append([first_guess, hint])
+        new_ans = answers_guess_hint_to_answers(new_ans, first_guess, hint)
+        
+    while len(new_ans) > 1:
+        guess = least_squares(new_ans, GUESSES)[0]
+        hint = guess_to_hint(answer, guess)
+        gh.append([guess, hint])
+        new_ans = answers_guess_hint_to_answers(new_ans, guess, hint)
+    final = [new_ans[0], GREEN * 5]
+    gh.append(final)
+    return gh
+
 
 if __name__ == "__main__":
-    print(least_squares(SOLUTIONS, GUESSES))
-
-    
-# generalize the below to find the best second guesses after ROATE or ARISE
-# new_ans = answers_guess_hint_to_answers(SOLUTIONS, 'ROATE', GRAY * 5)
-# new_best_guess = least_squares(new_ans, GUESSES)
-# print(new_best_guess)
-# input()
+    print(solution_path('DOING', 'SULCI'))
